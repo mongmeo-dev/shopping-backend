71efa159763172105eced8c1dfbc85b1af51899f
mongmeo-dev <mongmeo.dev@gmail.com>
2022-05-23 03:21:55 +0900

    implement user update, delete

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -5,6 +5,7 @@ import { CurrentUserId } from '../common/decorators/current-user.decorator';
 import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from '../auth/gql-auth.guard';
 import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -21,5 +22,21 @@ export class UsersResolver {
   @UseGuards(GqlAuthGuard)
   async loggedInUser(@CurrentUserId() id: number): Promise<User> {
     return await this.usersService.getUserById(id);
+  }
+
+  @Mutation(() => User)
+  @UseGuards(GqlAuthGuard)
+  async updateLoggedInUser(
+    @CurrentUserId() id: number,
+    @Args({ name: 'updateUserData' }) updateUserData: UpdateUserInput,
+  ): Promise<User> {
+    return await this.usersService.updateUserById(id, updateUserData);
+  }
+
+  @Mutation(() => Boolean)
+  @UseGuards(GqlAuthGuard)
+  async deleteLoggedInUser(@CurrentUserId() id: number): Promise<boolean> {
+    await this.usersService.deleteUserById(id);
+    return true;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,12 +4,13 @@ import { User } from './model/user.model';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
 
-  async join(createUserData: CreateUserInput) {
+  async join(createUserData: CreateUserInput): Promise<User> {
     const { email, password, phone } = createUserData;
     await this.checkOverlap(email, phone);
 
@@ -22,7 +23,7 @@ export class UsersService {
     return await this.usersRepository.save(newUser);
   }
 
-  async getUserByEmail(email: string) {
+  async getUserByEmail(email: string): Promise<User> {
     const user = await this.usersRepository.findOne({ where: { email } });
     if (!user) {
       throw new NotFoundException('유저가 존재하지 않습니다.');
@@ -30,7 +31,26 @@ export class UsersService {
     return user;
   }
 
-  async getUserById(id: number) {
+  async getUserById(id: number): Promise<User> {
+    return await this.getUserByIdOrThrow404(id);
+  }
+
+  async updateUserById(id: number, updateUserData: UpdateUserInput): Promise<User> {
+    const user = await this.getUserByIdOrThrow404(id);
+    const { email, phone } = updateUserData;
+
+    if (email || phone) {
+      await this.checkOverlap(updateUserData.email, updateUserData.phone);
+    }
+    return await this.usersRepository.save({ ...user, ...updateUserData });
+  }
+
+  async deleteUserById(id: number): Promise<void> {
+    await this.getUserByIdOrThrow404(id);
+    await this.usersRepository.delete({ id });
+  }
+
+  private async getUserByIdOrThrow404(id: number): Promise<User> {
     const user = await this.usersRepository.findOne({ id });
     if (!user) {
       throw new NotFoundException('유저가 존재하지 않습니다.');
@@ -38,7 +58,7 @@ export class UsersService {
     return user;
   }
 
-  private async checkOverlap(email: string, phone: string) {
+  private async checkOverlap(email: string, phone: string): Promise<void> {
     let isExist = await this.usersRepository.findOne({
       where: { email },
     });
