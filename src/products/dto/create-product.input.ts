93ca4109dc8224835b198641a579374b89d76f48
mongmeo-dev <mongmeo.dev@gmail.com>
2022-05-26 06:46:54 +0900

    add class valitaion in create product input

--- src/products/dto/create-product.input.ts
+++ src/products/dto/create-product.input.ts
@@ -1,16 +1,24 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsNotEmpty, IsPositive, IsString } from 'class-validator';
 
 @InputType()
 export class CreateProductInput {
+  @IsString()
+  @IsNotEmpty()
   @Field(() => String)
   name: string;
 
+  @IsPositive()
   @Field(() => Int)
   price: number;
 
+  @IsPositive()
+  @IsNotEmpty()
   @Field(() => Int)
   stock: number;
 
+  @IsString()
+  @IsNotEmpty()
   @Field(() => String)
   detail: string;
 
